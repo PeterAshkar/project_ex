c5e8b94223a90d73df41ffd779e8b009a1fd3685
peter <peter@10.0.0.7>
2024-01-15 10:34:32 +0200

    first attempt at commit

---
+++ ex1_lectrure2/src/main/java/ex1_lecture2/ex1_lectrure2/App.java
@@ -0,0 +1,13 @@
+package ex1_lecture2.ex1_lectrure2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
